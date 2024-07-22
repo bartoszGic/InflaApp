231f3fb1b8449713a521c0eaed18a6b427cd5b88
Bartosz <bartosz.gicala@gmail.com>
2024-07-22 20:05:48 +0200

    Added backend_v2

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,32 +7,23 @@ let articles = [];
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-});
-
 app.get('/', (req, res) => {
-    console.log('GET /');
     res.json(articles);
 });
 
 app.post('/', (req, res) => {
-    console.log('POST /', req.body);
     const article = req.body;
     articles.push(article);
     res.status(201).send('Article added');
 });
 
 app.delete('/:id', (req, res) => {
-    console.log(`DELETE /${req.params.id}`);
     const id = parseInt(req.params.id);
     articles = articles.filter(article => article.id !== id);
     res.send('Article deleted');
 });
 
 app.put('/:id', (req, res) => {
-    console.log(`PUT /${req.params.id}`, req.body);
     const id = parseInt(req.params.id);
     const updatedArticle = req.body;
     articles = articles.map(article => {
@@ -46,3 +37,9 @@ app.put('/:id', (req, res) => {
     });
     res.send('Article updated');
 });
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
+module.exports = app;
